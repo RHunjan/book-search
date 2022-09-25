efae16691003da21734fc8a4a7c565c966a3e998
ReenaH <Reena_Hunjan@yahoo.com>
2022-09-25 17:51:10 -0400

    resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,9 +5,22 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    me: async () => {
-      return User.findOne().sort({ createdAt: -1 });
-    }
+
+me: async (parent, args, context) => {
+  if (context.user) {
+    const userData = await User.findOne({ _id: context.user._id })
+      .select('-__v -password')
+      .populate('thoughts')
+      .populate('friends');
+
+    return userData;
+  }
+
+  throw new AuthenticationError('Not logged in');
+}
+
+
+
   },
     Mutation: {
       addUser: async (parent, args) => {
